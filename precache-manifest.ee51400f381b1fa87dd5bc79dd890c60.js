286ec95c2bfd20df99026c5ffcf1d8205044bb8b
paragjjw <pjjw30@gmail.com>
2022-06-12 11:28:11 +0530

    Updates

---
+++ precache-manifest.ee51400f381b1fa87dd5bc79dd890c60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "39275febd0344537e534",
+    "url": "/react-practice/static/css/main.56a3b558.chunk.css"
+  },
+  {
+    "revision": "39275febd0344537e534",
+    "url": "/react-practice/static/js/main.39275feb.chunk.js"
+  },
+  {
+    "revision": "5abdabad2e736bd78246",
+    "url": "/react-practice/static/js/1.5abdabad.chunk.js"
+  },
+  {
+    "revision": "5038283ee8b31ad665ab",
+    "url": "/react-practice/static/js/2.5038283e.chunk.js"
+  },
+  {
+    "revision": "de7d38c9961144acd298",
+    "url": "/react-practice/static/js/runtime~main.de7d38c9.js"
+  },
+  {
+    "revision": "1972b0972c711b302f9c439890c21fab",
+    "url": "/react-practice/index.html"
+  }
+];
